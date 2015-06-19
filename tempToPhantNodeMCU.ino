01632155e02c9f92cb932a7d675ff7cf63942458
fcspiteri <fcspiteri@gmail.com>
2015-06-19 06:55:40 -0700

    Update tempToPhantNodeMCU.ino

--- tempToPhantNodeMCU.ino
+++ tempToPhantNodeMCU.ino
@@ -9,8 +9,8 @@ int softTX = 5;	// TX pin for software serial
 int softRX = 6;	// RX pin for software serial
 
 // Phant server info
-String phantPubKey = "YOURPHANTPUBLICKEY";
-String phantPrivKey = "YOURPHANTPRIVATEKEY";
+String phantPubKey = "EJEaAXMEVGT5QAjO8gj0";
+String phantPrivKey = "dqM6ma5MKWCy9BAX6EAk";
 String phantField = "temp";
 String serverIP = "54.86.132.254"; // data.sparkfun.com IP address
 String serverPort = "80";
@@ -34,23 +34,30 @@ void loop(){
 
 	// Measure temperature
 	String tempC = measureTempF();
+        Serial.println(tempC);
 
 	// Concatenate GET string
-	String getStr = "GET /input/" + phantPubKey + "/?private_key=" + phantPrivKey + "&" + phantField + "=" + tempC + " HTTP/1.1\r\nHost: " + serverIP + "\r\nAccept: */*";
-	
+	String getStr = "GET /input/" + phantPubKey + "/?private_key=" + phantPrivKey + "&" + phantField + "=" + tempC + " HTTP/1.1\\r\\nHost: " + serverIP + "\\r\\nAccept: */*\\r\\n\\r\\n";
+	Serial.println(getStr);
+
 	// Create connection to server
 	ser.println("conn=net.createConnection(net.TCP, false)");
+	Serial.println("conn=net.createConnection(net.TCP, false)");
 	delay(500);
-	ser.println("conn:on("receive", function(conn, pl) print(pl) end)");
+	ser.println("conn:on(\"receive\", function(conn, pl) print(pl) end)");
+	Serial.println("conn:on(\"receive\", function(conn, pl) print(pl) end)");
+        delay(500);
 	ser.println("conn:connect(80,\"" + serverIP + "\")");
+	Serial.println("conn:connect(80,\"" + serverIP + "\")");
 	delay(5000);
         
 	// Send data
 	ser.println("conn:send(\"" + getStr + "\")");
+	Serial.println("conn:send(\"" + getStr + "\")");
 	delay(5000);
          
 	// Delay between updates
-	delay(60000);
+	delay(30000);
 }
 
 String measureTempF(){
